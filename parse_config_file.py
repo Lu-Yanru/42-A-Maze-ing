7afb4506a751e1ce42670933f595fef3385eff42
Lucas Vasconcelos <lvasconc@c4c3c6.42berlin.de>
2026-02-04 11:11:35 +0100

    parsing update 2.1

--- parse_config_file.py
+++ parse_config_file.py
@@ -1,16 +1,19 @@
 # Parsing config file #
 # Takes file name and returns Dict with parsed values #
 def parse_config_file(file_name: str) -> dict:
-    file = open(file_name)
-    lines = []
-    lines = file.read().split("\n")
+    try:
+        file = open(file_name, "r")
+        lines = []
+        lines = file.read().split("\n")
 
-    defines = {}
+        defines = {}
 
-    for line in lines:
-        if "=" in line:
-            key = line.split("=")[0]
-            value = line.split("=")[1]
-            defines[key] = value
-    print(defines)
-    return defines
+        for line in lines:
+            if "=" in line:
+                key = line.split("=")[0]
+                value = line.split("=")[1]
+                defines[key] = value
+        print(defines)
+        return defines
+    except FileNotFoundError:
+        print(f"ERROR... file: {file_name} not found!")
